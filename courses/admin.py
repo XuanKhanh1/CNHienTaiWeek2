7f8adb2b5217f70e813996c4899e1cac176dca1e
xuka <khanhtnx@gmail.com>
2024-11-10 16:16:04 +0700

    update

---
+++ courses/admin.py
@@ -0,0 +1,57 @@
+from django.contrib import admin
+from django.db.models import Count
+from django.template.response import TemplateResponse
+
+from courses.models import Category, Course, Lesson
+from django.utils.html import mark_safe
+from django.urls import path
+from django import forms
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
+
+
+class MyCourseAdmin(admin.AdminSite):
+    site_header = 'OU eCourse'
+
+
+    def get_urls(self):
+        return [path('course-stats/',self.statas)] + super().get_urls()
+
+
+    def statas(self, request):
+        # đếm số khóa học của danh mục
+        stats = Category.objects.annotate(count=Count('course__id')).values('id', 'name', 'count')
+
+        return TemplateResponse(request, 'admin/stats.html', {
+            'stats': stats
+    })
+
+
+class LessonForm(forms.ModelForm):
+    content = forms.CharField(widget=CKEditorUploadingWidget)
+
+    class Meta:
+        model = Lesson
+        fields = '__all__'
+
+
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ['id', 'subject', 'active', 'created_date']
+    search_fields = ['subject']
+    list_filter = ['id', 'subject', 'created_date']
+    readonly_fields = ['avatar']
+    form = LessonForm
+
+    def avatar(self, lesson):
+        return mark_safe(f"<img src='/static/{lesson.image.name}' width='120' />")
+
+    class Media:
+        css = {
+            'all': ('/static/css/styles.css',)
+        }
+
+
+admin_site = MyCourseAdmin(name='eCourse')
+
+admin_site.register(Category)
+admin_site.register(Course)
+admin_site.register(Lesson, LessonAdmin)
